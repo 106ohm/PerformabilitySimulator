9351941c0e0111fc7abca1c07814c6d64090c85c
Giulio Masetti <giulio.masetti@isti.cnr.it>
2021-05-30 21:44:27 +0200

    Introduce higher oder moments in scripts/conditional_accumulated_reward.R

--- scripts/conditional_accumulated_reward.R
+++ scripts/conditional_accumulated_reward.R
@@ -1,3 +1,8 @@
+library(e1071)
 table = read.csv("results_ConditionalAccumulatedReward.csv")
 selected = table[table$V==1,]
 mean(selected$Y)
+moment(selected$Y,order=2,center=FALSE)
+moment(selected$Y,order=3,center=FALSE)
+moment(selected$Y,order=4,center=FALSE)
+
